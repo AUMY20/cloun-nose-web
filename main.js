97bafe94dda565d07ec249fced48be06725eccb5
AUMY20 <99035834+AUMY20@users.noreply.github.com>
2022-08-08 19:58:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+nose_x = 0;
+nose_y = 0;
+
+function preload()
+{
+    nose = loadImage('clown-nose.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+    cam = createCapture(VIDEO);
+    cam.size(300,300) ;cam.hide();
+    union = ml5.poseNet(cam,modelLoded);
+    union.on('pose',gotPoses);
+}
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+        console.log(results);
+        nose_x = results[0].pose.nose.x;
+        nose_y = results[0].pose.nose.y;
+    }
+}
+
+function modelLoded()
+{
+console.log("check");
+}
+
+function draw()
+{
+    image(cam,0,0,300,300);
+    //!#clown nose done manually#!
+    //fill(255,150,0);
+    // stroke(255,0,0);
+    // circle(nose_x,nose_y,20);
+    //clown noseusing downloaded image
+    image(nose,nose_x-15,nose_y-15,40,40);
+
+}
+
+function take_snapshot()
+{
+    save('JokER.png');
+}
